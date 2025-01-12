fbd7da8264c2a33dc718e6aaf732e9547492d4c5
Shahzab Asif <shahzab@artemissoftware.org>
2025-01-13 01:29:45 +0400

    first commit

--- app/layout.js
+++ app/layout.js
@@ -1,26 +1,34 @@
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import "./global.scss";
+import styles from "./styles.module.scss";
 
 export const metadata = {
   title: "Artemis Fullstack Developer Task",
 };
 
-export default function RootLayout({ children }) {
+const RootLayout = ({ children, title, subtitle }) => {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+      <body>
+        <main className={styles.layout}>
+          <aside className={styles.sidebar}>
+            <div className={styles.logo}>
+              <img src="/images/logo2.svg" alt="logo" />
+            </div>
+            <div>
+              <h1>{title}</h1>
+              <p>{subtitle}</p>
+            </div>
+          </aside>
+          <section className={styles.content}>
+            <div className={styles.logo}>
+              <img src="/images/logo1.svg" alt="logo" />
+            </div>
+            <div className={styles.contentHeader}>{children}</div>
+          </section>
+        </main>
       </body>
     </html>
   );
-}
+};
+
+export default RootLayout;
